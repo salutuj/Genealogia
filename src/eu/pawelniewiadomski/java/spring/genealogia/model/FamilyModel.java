be8e7cb62be42762424d97d95d8c2b31e54f3779
Paweł Niewiadomski <pawel.niewiadomski@hybris.com>
2016-07-27 14:22:55 +0200

    not commited in last revision?

--- src/eu/pawelniewiadomski/java/spring/genealogia/model/FamilyModel.java
+++ src/eu/pawelniewiadomski/java/spring/genealogia/model/FamilyModel.java
@@ -2,4 +2,18 @@ package eu.pawelniewiadomski.java.spring.genealogia.model;
 
 public class FamilyModel extends AbstractModel{
 
+  public String getId() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  public Object getFather() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+  public Object getMother() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+  
 }
