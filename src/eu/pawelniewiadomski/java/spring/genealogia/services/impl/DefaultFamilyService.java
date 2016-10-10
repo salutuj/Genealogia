94d203337163d385a9c5642fba33cf6cf670c0d2
Pawel Niewiadomski <salutuj@tlen.pl>
2016-10-10 23:25:16 +0200

    more changes

--- src/eu/pawelniewiadomski/java/spring/genealogia/services/impl/DefaultFamilyService.java
+++ src/eu/pawelniewiadomski/java/spring/genealogia/services/impl/DefaultFamilyService.java
@@ -20,37 +20,44 @@ import eu.pawelniewiadomski.java.spring.genealogia.dao.FamilyDao;
 import eu.pawelniewiadomski.java.spring.genealogia.model.FamilyModel;
 import eu.pawelniewiadomski.java.spring.genealogia.model.PersonModel;
 import eu.pawelniewiadomski.java.spring.genealogia.model.gedcom.GedcomFamilyModel;
+import eu.pawelniewiadomski.java.spring.genealogia.model.gedcom.GedcomIndividualModel;
 import eu.pawelniewiadomski.java.spring.genealogia.services.FamilyService;
 import eu.pawelniewiadomski.java.spring.genealogia.services.PersonService;
 
-public class DefaultFamilyService implements FamilyService{
+public class DefaultFamilyService implements FamilyService {
 
   protected static final Log LOG = LogFactory.getLog(DefaultFamilyService.class);
-  
-  private FamilyDao familyDao;  
-  private PersonService personService;
-  
-	@Override
-	public FamilyModel findFamilyById(String id) {
-		// TODO Auto-generated method stub
-		return null;
-	}
 
-	@Override
-	public Collection<FamilyModel> findAllFamilies() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+  protected FamilyDao familyDao;
+  protected PersonService personService;
 
-	@Override
-	public FamilyModel getDefaultFamily() {	  	  
-    GedcomFamilyModel defaultGedcomFamily = familyDao.findFamilyById("1");
-    if ( defaultGedcomFamily == null)
-      throw new NullPointerException("family is null");
-    return convertGedcomToFamily( defaultGedcomFamily);
-	}
-	
-  public FamilyModel convertGedcomToFamily(GedcomFamilyModel gedcomFamily){
+  @Override
+  public FamilyModel findFamilyById(String familyId) {
+    if ( familyId == null){
+      LOG.error("Family id is null");
+      return null;
+    }    
+    GedcomFamilyModel gedcomFamily= getFamilyDao().findFamilyById(familyId);
+    return convertGedcomToFamily(gedcomFamily);
+  }
+
+  @Override
+  public Collection<FamilyModel> findAllFamilies() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public FamilyModel getDefaultFamily() {
+    GedcomFamilyModel defaultGedcomFamily = getFamilyDao().findFamilyById("F1");
+    if (defaultGedcomFamily == null) {
+      LOG.error("defaultGedcomFamily is null");
+      return null;
+    }
+    return convertGedcomToFamily(defaultGedcomFamily);
+  }
+
+  public FamilyModel convertGedcomToFamily(GedcomFamilyModel gedcomFamily) {
     FamilyModel familyModel = new FamilyModel();
     familyModel.setId(gedcomFamily.getId());
     GedcomParser parser = new GedcomParser();
@@ -59,35 +66,52 @@ public class DefaultFamilyService implements FamilyService{
     } catch (GedcomParserException e) {
       LOG.error("Incorrect syntax in gedcom string", e);
       return null;
-    } catch (IOException e) {      
+    } catch (IOException e) {
       LOG.error("IOException occured when parsing gedcom data", e);
       return null;
     }
     Map<String, Family> gedcomFamilies = parser.getGedcom().getFamilies();
-    if ( gedcomFamilies == null || gedcomFamilies.isEmpty()){
+    if (gedcomFamilies == null || gedcomFamilies.isEmpty()) {
       LOG.error("No families found in gedcom data. At least one must is expected");
     }
-    Family family = gedcomFamilies.get(id2Xref(gedcomFamily.getId()));    
-    //TODO familyModel.setFamilyName(family.toString());        
-    familyModel.setFather(personService.findPersonById(gedcomFamily.getHusbandId()));            
-    familyModel.setMother(personService.findPersonById(gedcomFamily.getWifeId()));
+    Family family = gedcomFamilies.get(id2Xref(gedcomFamily.getId()));
+    // TODO familyModel.setFamilyName(family.toString());
+    familyModel.setFather(getPersonService().findPersonById(gedcomFamily.getHusbandId()));
+    familyModel.setMother(getPersonService().findPersonById(gedcomFamily.getWifeId()));
     List<Individual> children = family.getChildren();
-    if ( children == null || children.isEmpty())
+    if (children == null || children.isEmpty())
       familyModel.setChildren(Collections.emptyList());
-    else{
+    else {
       Collection<PersonModel> childrenModels = new ArrayList<PersonModel>();
       for (Individual child : children)
-        childrenModels.add(personService.findPersonById(xref2Id(child.getXref())));
+        childrenModels.add(getPersonService().findPersonById(xref2Id(child.getXref())));
       familyModel.setChildren(childrenModels);
     }
     return familyModel;
   }
 
-  private String id2Xref(String id){
+  private String id2Xref(String id) {
     return "@" + id + "@";
   }
-  
-  private String xref2Id(String xref){
+
+  private String xref2Id(String xref) {
     return xref.split("@")[1];
   }
+
+  public void setPersonService(PersonService personService) {
+    this.personService = personService;
+  }
+
+  public PersonService getPersonService() {
+    return personService;
+  }
+
+  public void setFamilyDao(FamilyDao familyDao) {
+    this.familyDao = familyDao;
+  }
+
+  public FamilyDao getFamilyDao() {
+    return familyDao;
+  }
+
 }
